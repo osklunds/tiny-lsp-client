0080950e6cd4e99fcbefea81b3cc5bb7b90e57c2
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2025-01-22 20:23:42 +0100

    Clean up tests

--- src/connection/tests.rs
+++ src/connection/tests.rs
@@ -13,63 +13,6 @@ fn initialize() {
 }
 
 #[test]
-fn text_document_definition() {
-    let mut connection = Connection::new(
-        "rust-analyzer",
-        "/home/oskar/own_repos/tiny-lsp-client"
-    );
-
-    connection.initialize();
-
-    let params = DefinitionParams {
-        text_document: TextDocumentIdentifier {
-            uri: "file:///home/oskar/own_repos/tiny-lsp-client/src/dummy.rs".to_string()
-        },
-        position: Position {
-            character: 4,
-            line: 4
-        }
-    };
-
-    let params = RequestParams::DefinitionParams(params);
-
-    let mut result: Vec<LocationLink> = vec![];
-    loop {
-        // this might have to return id, to be used when comparing recv
-        // recv should not return Message. Just (usize, Response)
-        connection.send_request(params.clone());
-        let response = connection.recv_response();
-
-        // rust-analyzer isn't ready to respond to the request immediately due
-        // to it indexing first. So try until succeeds.  rust-analyzer responds
-        // with different results before it's ready depending on timing.
-        if let Some(Result::TextDocumentDefinitionResult(r)) = &response.result {
-            if let DefinitionResult::LocationLinkList(r) = r {
-                if !r.is_empty() {
-                    result = r.to_vec();
-                    break;
-                }
-            }
-        }
-
-        thread::sleep(Duration::from_millis(100));
-    }
-
-    let location_link = &result[0];
-    let target_range = &location_link.target_range;
-    let start = &target_range.start;
-    let end = &target_range.end;
-    assert!(start.line == 7);
-    assert!(start.character == 0);
-    assert!(end.line == 9);
-    assert!(end.character == 1);
-
-    // Send a second identical request and get a response immediately
-    connection.send_request(params);
-    connection.recv_response();
-}
-
-#[test]
 fn did_open_change_close_and_definition() {
     let mut connection = Connection::new(
         "rust-analyzer",
@@ -77,8 +20,9 @@ fn did_open_change_close_and_definition() {
     );
     connection.initialize();
 
-    thread::sleep(Duration::from_millis(1000));
-    // Send the notification afterwards to know that rust-analyzer is ready
+    // let rust-analyzer have time to start
+    // todo: consider looping again
+    thread::sleep(Duration::from_millis(2000));
 
     let uri = "file:///home/oskar/own_repos/tiny-lsp-client/src/dummy.rs".to_string();
 
@@ -97,8 +41,6 @@ fn did_open_change_close_and_definition() {
     };
     let did_open = Message::Notification(did_open);
     connection.send_msg(did_open);
-
-    thread::sleep(Duration::from_millis(1000));
 
     let definition_params = DefinitionParams {
         text_document: TextDocumentIdentifier {
@@ -166,6 +108,7 @@ fn did_open_change_close_and_definition() {
     connection.send_msg(did_change);
     thread::sleep(Duration::from_millis(1000));
 
+    // Ask for same position
     let definition_params = DefinitionParams {
         text_document: TextDocumentIdentifier {
             uri: uri.clone()
@@ -184,13 +127,13 @@ fn did_open_change_close_and_definition() {
     let definition = Message::Request(definition);
     connection.send_msg(definition);
     let response = connection.recv_response();
-    thread::sleep(Duration::from_millis(1000));
     let result = response.result.unwrap();
     let Result::TextDocumentDefinitionResult(result) = result else { panic!() };
     let DefinitionResult::LocationLinkList(result) = result else { panic!() };
     assert_eq!(1, result.len());
     let location_link = result[0].clone();
     let target_range = &location_link.target_range;
+    // Due to change, different position
     let exp_target_range = Range {
         start: Position {
             line: 8,
