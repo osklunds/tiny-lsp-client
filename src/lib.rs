d2883e2ec21e06d30b55c17f6c37210a1271d849
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2025-01-28 20:25:55 +0100

    Start of emacs bindings

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+// Heavily inspired by https://github.com/zbelial/lspce
+
+#![allow(warnings)] 
+
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+mod learning_tests;
+mod dummy;
+mod connection;
+mod message;
+
+fn main() {
+
+}
+
