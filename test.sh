bf32bbb3262c941c0fff77d5b991076fcb315cc5
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2025-02-03 20:30:23 +0100

    Improve test shell scripp with batch mode and exit code check

--- test.sh
+++ test.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-cargo build && RUST_BACKTRACE=1 emacs -Q --eval '(load-file "tiny-lsp-client-test.el")'
+cargo build && RUST_BACKTRACE=1 emacs \
+    -Q \
+    --batch \
+    --eval '(load-file "tiny-lsp-client-test.el")'
+
+code=$?
+
+echo ""
+echo ""
+echo ""
+
+if [ $code -eq 0 ]; then
+    echo "$OK"
+else
+    echo "$FAILED"
+fi
