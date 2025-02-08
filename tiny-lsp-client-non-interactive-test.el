39e44fbbf68a18fc7cc4ebf774d7fe4844ec7bbf
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2025-02-08 20:13:21 +0100

    Find definition in non-interactive test

--- tiny-lsp-client-non-interactive-test.el
+++ tiny-lsp-client-non-interactive-test.el
@@ -1,4 +1,13 @@
 
+(defun std-message (format-string &rest args)
+  (print (format (concat "[emacs]  " format-string) args) 'external-debugging-output))
+
+(defun assert-equal (exp act)
+  (unless (equal exp act)
+    (std-message "Exp %s" exp)
+    (std-message "Act %s" act)
+    (cl-assert (equal exp act) 'show)))
+
 (add-to-list 'load-path default-directory)
 
 (define-derived-mode rust-mode prog-mode "Rust"
@@ -11,4 +20,20 @@
 (add-hook 'rust-mode-hook 'tlc-mode)
 
 (find-file "src/dummy.rs")
+
+(assert-equal 'rust-mode major-mode)
+(assert-equal t tlc-mode)
+(assert-equal '(tlc-xref-backend t) xref-backend-functions)
+
+(re-search-forward "second_function")
+(assert-equal 5 (line-number-at-pos))
+(assert-equal 19 (current-column))
+
+(sleep-for 10)
+
+(let ((xref-prompt-for-identifier nil))
+  (call-interactively 'xref-find-definitions))
+
+(assert-equal 8 (line-number-at-pos))
+(assert-equal 3 (current-column))
 
