5372f5338775003f060b1c8e3b9f14ae65a02df9
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2025-02-09 20:01:03 +0100

    Prepend IO/DEBUG

--- src/logger.rs
+++ src/logger.rs
@@ -22,19 +22,15 @@ pub static LOG_TO_STDIO: AtomicBool = AtomicBool::new(true);
 pub fn log_io_fun<S: AsRef<str>>(msg: S) {
     let timestamp = Local::now().format("%Y-%m-%d %H:%M:%S%.3f");
 
-    let formatted_msg = format!("{} - {}\n", timestamp, msg.as_ref());
-
     if LOG_TO_STDIO.load(Ordering::Relaxed) {
-        print!("{}", formatted_msg);
+        print!("IO {} - {}\n", timestamp, msg.as_ref());
     }
 }
 
 pub fn log_debug_fun<S: AsRef<str>>(msg: S) {
     let timestamp = Local::now().format("%Y-%m-%d %H:%M:%S%.3f");
 
-    let formatted_msg = format!("{} - {}\n", timestamp, msg.as_ref());
-
     if LOG_TO_STDIO.load(Ordering::Relaxed) {
-        print!("{}", formatted_msg);
+        print!("DEBUG {} - {}\n", timestamp, msg.as_ref());
     }
 }
