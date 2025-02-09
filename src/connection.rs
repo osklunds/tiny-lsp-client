18f0b4fdadc8ec188a9738aae179e5d2589b0c72
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2025-02-09 19:53:01 +0100

    Add log_io macro

--- src/connection.rs
+++ src/connection.rs
@@ -2,7 +2,7 @@
 mod tests;
 
 use crate::message::*;
-use crate::logger;
+use crate::logger::log_io;
 
 use serde::Deserialize;
 use serde::Serialize;
@@ -49,8 +49,7 @@ impl Connection {
                 let full =
                     format!("Content-Length: {}\r\n\r\n{}", json.len(), &json);
                 stdin.write(full.as_bytes()).unwrap();
-                logger::log_io(format!("Sent: {}",
-                                       serde_json::to_string_pretty(&msg).unwrap()));
+                log_io!("Sent: {}", serde_json::to_string_pretty(&msg).unwrap());
             } else {
                 return;
             }
@@ -86,7 +85,7 @@ impl Connection {
                 // Decode as serde_json::Value too, to be able to print fields
                 // not deserialized into msg.
                 let full_json: serde_json::Value = serde_json::from_str(&json).unwrap();
-                logger::log_io(format!("Received: {}", serde_json::to_string_pretty(&full_json).unwrap()));
+                log_io!("Received: {}", serde_json::to_string_pretty(&full_json).unwrap());
 
                 let msg = serde_json::from_str(&json).unwrap();
 
--- src/logger.rs
+++ src/logger.rs
@@ -3,9 +3,16 @@ use std::sync::atomic::AtomicBool;
 use std::sync::atomic::Ordering;
 use chrono::Local;
 
+macro_rules! log_io {
+    ($($arg:tt)*) => {
+        crate::logger::log_io_fun(format!($($arg)*));
+    }
+}
+pub(crate) use log_io;
+
 pub static LOG_TO_STDIO: AtomicBool = AtomicBool::new(true);
 
-pub fn log_io<S: AsRef<str>>(msg: S) {
+pub fn log_io_fun<S: AsRef<str>>(msg: S) {
     let timestamp = Local::now().format("%Y-%m-%d %H:%M:%S%.3f");
 
     let formatted_msg = format!("{} - {}\n", timestamp, msg.as_ref());
