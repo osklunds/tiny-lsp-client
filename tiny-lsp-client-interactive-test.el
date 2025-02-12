140ec3f8bea9bd952a7d0aff5f99e265ffef91b3
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2025-02-12 20:06:20 +0100

    Set all log opts to true in interactive test

--- tiny-lsp-client-interactive-test.el
+++ tiny-lsp-client-interactive-test.el
@@ -16,3 +16,8 @@
 (add-hook 'rust-mode-hook 'tlc-mode)
 
 (find-file "src/dummy.rs")
+
+(customize-set-variable 'tlc-log-io t)
+(customize-set-variable 'tlc-log-stderr t)
+(customize-set-variable 'tlc-log-debug t)
+(customize-set-variable 'tlc-log-to-stdio t)
