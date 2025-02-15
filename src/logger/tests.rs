516aea741317ae8e355d5f72c65a9d5e02c586c3
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2025-02-15 20:21:10 +0100

    Test millisecond output

--- src/logger/tests.rs
+++ src/logger/tests.rs
@@ -9,8 +9,9 @@ fn timestamp_test() {
     println!("Timestamp: {}", timestamp);
     // No need to make it overly smart, because no matter how smart,
     // it will never be able to compare against the real time with a regex
-    let regex =
-        Regex::new(r"2[0-9]{3}-[01][0-9]-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}")
-            .unwrap();
+    let regex = Regex::new(
+        r"^2[0-9]{3}-[01][0-9]-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}\.[0-9]{3}$",
+    )
+    .unwrap();
     assert!(regex.is_match(&timestamp));
 }
