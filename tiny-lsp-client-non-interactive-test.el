09b3d804f038a1c7c1164901e277a788ed0bd384
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2025-02-15 08:46:41 +0100

    Enable emacs debug also in non interactive test

--- tiny-lsp-client-non-interactive-test.el
+++ tiny-lsp-client-non-interactive-test.el
@@ -39,6 +39,7 @@
 (customize-set-variable 'tlc-log-io t)
 (customize-set-variable 'tlc-log-stderr t)
 (customize-set-variable 'tlc-log-debug t)
+(customize-set-variable 'tlc-log-emacs-debug t)
 (customize-set-variable 'tlc-log-to-stdio t)
 
 (add-hook 'rust-mode-hook 'tlc-mode)
