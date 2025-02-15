cff7690d6f22017973fc5504a6a6c9ef520c609a
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2025-02-15 13:56:47 +0100

    Split build and assert repo to separate funs

--- src/tests.rs
+++ src/tests.rs
@@ -3,13 +3,18 @@ use std::path::Component;
 
 #[test]
 fn hej() {
+    build();
+}
+
+fn build() {
+    let mut build = Command::new("cargo").args(vec!["build"]).spawn().unwrap();
+    assert!(build.wait().unwrap().success());
+}
+
+fn assert_in_repo() {
     // Test that current dir seems to be the root of the repo
     let current_dir = std::env::current_dir().unwrap();
     let path_components: Vec<_> = current_dir.components().collect();
     let last = path_components.last().unwrap();
     assert_eq!(&Component::Normal("tiny-lsp-client".as_ref()), last);
-
-    let mut build = Command::new("cargo").args(vec!["build"]).spawn().unwrap();
-
-    assert!(build.wait().unwrap().success());
 }
