a736babbcfcbaba500d45df53d8766c46f69a07b
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2025-02-15 20:47:37 +0100

    Test more variants for millisecond padding

--- src/logger/tests.rs
+++ src/logger/tests.rs
@@ -18,22 +18,27 @@ fn timestamp() {
 
 #[test]
 fn millisecond_padding() {
-    // Test that padding seems to work correctly
-    let regex = Regex::new(r"\.00[1-9]$").unwrap();
-    loop {
-        let timestamp = get_timestamp();
-        if regex.is_match(&timestamp) {
-            println!("Timestamp: {}", timestamp);
-            break;
-        }
-    }
+    // Test that padding seems to work correctly by testing if they can occur at
+    // all.
 
-    let regex = Regex::new(r"\.[5-9]00$").unwrap();
-    loop {
-        let timestamp = get_timestamp();
-        if regex.is_match(&timestamp) {
-            println!("Timestamp: {}", timestamp);
-            break;
+    let regexs = vec![
+        r"\.000$",
+        r"\.[1-9]00$",
+        r"\.[1-9]{2}0$",
+        r"\.[1-9]{3}$",
+        r"\.0[1-9]{2}$",
+        r"\.00[1-9]$",
+        r"\.00[7-8]$", // 1s seem to be favored so force something else
+    ];
+
+    for regex in regexs {
+        let regex = Regex::new(regex).unwrap();
+        loop {
+            let timestamp = get_timestamp();
+            if regex.is_match(&timestamp) {
+                println!("Timestamp: {}", timestamp);
+                break;
+            }
         }
     }
 }
