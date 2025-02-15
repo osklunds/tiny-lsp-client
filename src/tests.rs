72add5318623f99ad5834dbd1494f16da5bdc9e6
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2025-02-15 13:55:28 +0100

    cargo build in test

--- src/tests.rs
+++ src/tests.rs
@@ -1,5 +1,15 @@
+use std::process::Command;
+use std::path::Component;
 
 #[test]
 fn hej() {
-    println!("oskar: {:?}", "hej");
+    // Test that current dir seems to be the root of the repo
+    let current_dir = std::env::current_dir().unwrap();
+    let path_components: Vec<_> = current_dir.components().collect();
+    let last = path_components.last().unwrap();
+    assert_eq!(&Component::Normal("tiny-lsp-client".as_ref()), last);
+
+    let mut build = Command::new("cargo").args(vec!["build"]).spawn().unwrap();
+
+    assert!(build.wait().unwrap().success());
 }
