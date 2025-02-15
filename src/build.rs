0e583ab02040d2d1edbd8326084e75346214da9f
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2025-02-15 20:37:43 +0100

    Revert "Rename to emacs_module_h_bindings"
    This reverts commit 9f379c0de0de4b317d7326393aff5fc7c33021d4.

--- src/build.rs
+++ src/build.rs
@@ -6,7 +6,7 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    let emacs_module_h_bindings = bindgen::Builder::default()
+    let bindings = bindgen::Builder::default()
         .header("/usr/include/emacs-module.h")
         .blocklist_function("emacs_module_init")
         .generate()
@@ -14,7 +14,7 @@ fn main() {
 
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
 
-    emacs_module_h_bindings
-        .write_to_file(out_path.join("emacs_module_h_bindings.rs"))
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
         .unwrap();
 }
