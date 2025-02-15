7eb1f9b36a2059f5ee505a48b7ae1817a3537f30
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2025-02-15 14:33:54 +0100

    Test that runs a lisp file

--- src/tests.rs
+++ src/tests.rs
@@ -1,20 +1,36 @@
-use std::process::Command;
 use std::path::Component;
+use std::process::Command;
 
 #[test]
 fn hej() {
     build(false);
     build(true);
+
+    run_lisp_file();
+}
+
+fn run_lisp_file() {
+    let mut args = vec![
+        "-Q",
+        "--batch",
+        "--eval",
+        "(load-file \"tiny-lsp-client-lisp-bindings-test.el\")",
+    ];
+
+    let mut child = Command::new("emacs").args(args).spawn().unwrap();
+    let status = child.wait().unwrap();
+    assert!(status.success());
 }
 
 fn build(release: bool) {
+    assert_in_repo();
     let mut args = vec!["build"];
     if release {
         args.push("--release");
     }
 
-    let mut build = Command::new("cargo").args(args).spawn().unwrap();
-    assert!(build.wait().unwrap().success());
+    let mut process = Command::new("cargo").args(args).spawn().unwrap();
+    assert!(process.wait().unwrap().success());
 }
 
 fn assert_in_repo() {
