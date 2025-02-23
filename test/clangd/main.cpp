10336da37f16e6e0b16110aac2730cae941315e2
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2025-02-23 06:49:36 +0100

    Add function in other file

--- test/clangd/main.cpp
+++ test/clangd/main.cpp
@@ -1,11 +1,12 @@
 
 #include <iostream>
+#include "other.hpp"
 
 void other_function(int arg) {
-    std::cout << "Howdy, other_function! " << arg << std::endl;
+    std::cout << arg << std::endl;
 }
 
 int main() {
     other_function(123);
-    std::cout << "Howdy, pardner!" << std::endl;
+    function_in_other_file();
 }
---
+++ test/clangd/other.cpp
@@ -0,0 +1,6 @@
+
+#include <iostream>
+
+void function_in_other_file() {
+    std::cout << "other" << std::endl;
+}
