acb4b61d7e8cf6907e377a84fd20e6003f90f811
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2025-02-23 14:56:14 +0100

    Migrate clangd tests to new style

--- test/new-test.el
+++ test/new-test.el
@@ -14,7 +14,7 @@
 ;; Common helpers
 ;; -----------------------------------------------------------------------------
 
-(defun my-assert-equal (exp act &optional label)
+(defun assert-equal (exp act &optional label)
   (when (not (equal exp act))
     (message "")
     (message "")
@@ -26,7 +26,9 @@
     (message "-----------------------------------------------------------------------------")
     (message "")
     (message "")
-    (message ""))
+    (message "")
+    (sleep-for 1)
+    )
   (should (equal exp act)))
 
 (defun run-shell-command (command &rest components)
@@ -38,7 +40,7 @@
                    (message (string-replace "%" "%%" (buffer-string)))
                    code)))
          (label (format "Command %s" command)))
-    (my-assert-equal 0 code label))
+    (assert-equal 0 code label))
   (message "Finished command '%s'" command)
   (message "-----------------------------------------------------------------------------")
   )
@@ -103,8 +105,22 @@
   (customize-set-variable 'tlc-log-file log-file-name))
 
 (defun after-each-test ()
-  (tlc-stop-server)
-  )
+  ;; One drawback of running in the same emacs instance with ERT is that
+  ;; this clean up in the end is needed.
+
+  ;; Only kill buffers visiting a file
+  (let ((buffers (cl-remove-if-not 'buffer-file-name (buffer-list))))
+    (dolist (buffer buffers)
+      (with-current-buffer buffer
+        ;; First disable tlc-mode so that kill-buffer doesn't trigger
+        ;; didClose
+        (tlc-mode -1)
+        ;; Stop server in a buffer where root path is known
+        (tlc-stop-server))
+      ;; Then kill the buffer too so that didOpen is sent if the same file
+      ;; is used in many tests
+      (kill-buffer buffer))
+    ))
 
 (cl-defmacro tlc-deftest (name () &rest body)
   (declare (indent defun))
@@ -119,19 +135,177 @@
 ;; Test cases
 ;; -----------------------------------------------------------------------------
 
-(tlc-deftest open ()
-  (my-assert-equal 0 (number-of-did-open))
-  (my-assert-equal 0 (number-of-did-close))
+(tlc-deftest open-a-file ()
+  ;; Arrange
+  (assert-equal 0 (number-of-did-open))
+  (assert-equal 0 (number-of-did-close))
+
+  (assert-equal nil tlc-mode)
+
+  ;; Act
+  (find-file (relative-repo-root "test" "clangd" "main.cpp"))
+
+  ;; Assert
+  (assert-equal 1 (number-of-did-open) "after")
+  (assert-equal 0 (number-of-did-close) "after")
+
+  (assert-equal 'c++-mode major-mode)
+  (assert-equal t tlc-mode)
+  (assert-equal '(tlc-xref-backend t) xref-backend-functions)
+  )
+
+(tlc-deftest find-definition ()
+  ;; Arrange
+  (assert-equal 0 (number-of-did-open))
+  (assert-equal 0 (number-of-did-close))
 
   (find-file (relative-repo-root "test" "clangd" "main.cpp"))
 
-  (my-assert-equal 1 (number-of-did-open))
-  (my-assert-equal 0 (number-of-did-close) "after")
+  (re-search-forward "other_function")
+  (re-search-forward "other_function")
+  (assert-equal 11 (line-number-at-pos))
+  (assert-equal 18 (current-column))
+
+  ;; Act
+  (non-interactive-xref-find-definitions)
+
+  ;; Assert
+  (assert-equal 5 (line-number-at-pos))
+  (assert-equal 6 (current-column))
+
+  (assert-equal 1 (number-of-did-open) "end")
+  (assert-equal 0 (number-of-did-close))
   )
 
-(tlc-deftest other-test ()
-  (my-assert-equal 0 (number-of-did-open))
-  (my-assert-equal 0 (number-of-did-close))
+(tlc-deftest edit ()
+  ;; Arrange
+  (assert-equal 0 (number-of-did-open))
+  (assert-equal 0 (number-of-did-close))
 
-  (find-file (relative-repo-root "test" "clangd" "other.cpp"))
+  (find-file (relative-repo-root "test" "clangd" "main.cpp"))
+
+  (assert-equal 
+   "
+#include <iostream>
+#include \"other.hpp\"
+
+short other_function(int arg) {
+    std::cout << arg << std::endl;
+    return 1;
+}
+
+int main() {
+    other_function(123);
+    function_in_other_file();
+}
+"
+   (current-buffer-string))
+  
+  ;; Act
+  (beginning-of-buffer)
+  (re-search-forward "other_function")
+  (insert "_")
+  (insert "h")
+  (insert "e")
+  (insert "j")
+
+  (re-search-forward "other_function")
+  (insert "_hej")
+
+  (assert-equal 
+   "
+#include <iostream>
+#include \"other.hpp\"
+
+short other_function_hej(int arg) {
+    std::cout << arg << std::endl;
+    return 1;
+}
+
+int main() {
+    other_function_hej(123);
+    function_in_other_file();
+}
+"
+   (current-buffer-string))
+
+  (beginning-of-buffer)
+  (re-search-forward "other_")
+  (re-search-forward "other_")
+  (assert-equal 11 (line-number-at-pos))
+  (assert-equal 10 (current-column))
+
+  ;; Assert
+
+  ;; Use find definition as a way to see that the contents seem synced
+  ;; todo: need to insert some newlines
+  (non-interactive-xref-find-definitions)
+  (assert-equal 5 (line-number-at-pos))
+  (assert-equal 6 (current-column))
+
+  ;; Act2
+  (beginning-of-buffer)
+  (re-search-forward "arg")
+  (backward-delete-char 2)
+  (re-search-forward "arg")
+  (beginning-of-line)
+  (replace-string "arg" "a")
+
+  (assert-equal 
+   "
+#include <iostream>
+#include \"other.hpp\"
+
+short other_function_hej(int a) {
+    std::cout << a << std::endl;
+    return 1;
+}
+
+int main() {
+    other_function_hej(123);
+    function_in_other_file();
+}
+"
+   (current-buffer-string))
+
+  (beginning-of-buffer)
+  (re-search-forward "<< a")
+  (backward-char)
+  (assert-equal 6 (line-number-at-pos))
+  (assert-equal 17 (current-column))
+
+  ;; Assert2
+
+  ;; Use find definition as a way to see that the contents seem synced
+  ;; todo: need to insert some newlines
+  (non-interactive-xref-find-definitions)
+  (assert-equal 5 (line-number-at-pos))
+  (assert-equal 29 (current-column))
+
+  (assert-equal 1 (number-of-did-open))
+  (assert-equal 0 (number-of-did-close))
+  )
+
+(tlc-deftest reverting-buffer ()
+  ;; Arrange
+  (assert-equal 0 (number-of-did-open))
+  (assert-equal 0 (number-of-did-close))
+
+  (find-file (relative-repo-root "test" "clangd" "main.cpp"))
+
+  (assert-equal 1 (number-of-did-open))
+  (assert-equal 0 (number-of-did-close))
+
+  (assert-equal 'c++-mode major-mode)
+  (assert-equal t tlc-mode)
+  (assert-equal '(tlc-xref-backend t) xref-backend-functions)
+
+  ;; Act
+  (revert-buffer nil 'no-confirm 'preserve-modes)
+
+  (assert-equal t tlc-mode)
+  (assert-equal '(tlc-xref-backend t) xref-backend-functions)
+
+  (assert-equal 2 (number-of-did-open))
+  (assert-equal 1 (number-of-did-close)) 
   )
