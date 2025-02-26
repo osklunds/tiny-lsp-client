1963d622a3456070c7b641786b0ca2caa055e575
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2025-02-26 20:10:25 +0100

    Add script for running all test files, one test file, or one test case

---
+++ test/run_tests.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [[ $# -eq 0 ]]; then
+    echo "Running all test files"
+    test_dir=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+    cd "$test_dir"
+    for file in *test.el; do
+        [[ -f "$file" ]] || break
+        echo "Running test file: $file"
+        emacs -batch -l ert -l "$file" --eval '(ert-run-tests-batch-and-exit)' || break
+    done
+fi
+
+if [[ $# -eq 1 ]]; then
+    echo "Running one test file: $1"
+    emacs -batch -l ert -l "$1" --eval '(ert-run-tests-batch-and-exit)'
+fi
+
+if [[ $# -eq 2 ]]; then
+    echo "Running test case $2 in test file: $1"
+    emacs -batch -l ert -l "$1" --eval "(ert-run-tests-batch-and-exit \"$2\")"
+fi
+
+if [[ $# -gt 2 ]]; then
+    echo "Wrong number of arguments"
+fi
+
