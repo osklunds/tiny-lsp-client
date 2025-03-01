692363855749a8992da41956a105fbf71de911a3
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2025-03-01 15:55:39 +0100

    Test insertText and textEdit too

--- src/message/tests.rs
+++ src/message/tests.rs
@@ -3,7 +3,7 @@ use super::*;
 use serde_json::json;
 
 #[test]
-fn completion_result() {
+fn completion_item_label() {
     let json: serde_json::Value = json!({
         "jsonrpc": 2.0,
         "id": 123,
@@ -35,4 +35,69 @@ fn assert_json_decodes_into(json: serde_json::Value, exp_decoded: Message) {
     let encoded: String = serde_json::to_string(&json).unwrap();
     let decoded: Message = serde_json::from_str(&encoded).unwrap();
     assert_eq!(exp_decoded, decoded);
+}
+
+#[test]
+fn completion_item_label_and_insert_text() {
+    let json: serde_json::Value = json!({
+        "jsonrpc": 2.0,
+        "id": 123,
+        "result": {
+            "items": [
+                {
+                    "label": "some_label",
+                    "insertText": "some_insert_text",
+                }
+            ]
+        }
+    });
+
+    let decoded = Message::Response(Response {
+        id: 123,
+        result: Some(Result::TextDocumentCompletionResult(CompletionResult {
+            items: vec![CompletionItem {
+                label: "some_label".to_string(),
+                insert_text: Some("some_insert_text".to_string()),
+                text_edit: None,
+            }],
+        })),
+        error: None,
+    });
+
+    assert_json_decodes_into(json, decoded);
+}
+
+#[test]
+fn completion_item_label_and_insert_text_and_text_edit() {
+    let json: serde_json::Value = json!({
+        "jsonrpc": 2.0,
+        "id": 123,
+        "result": {
+            "items": [
+                {
+                    "label": "some_label",
+                    "insertText": "some_insert_text",
+                    "textEdit": {
+                        "newText": "some_new_text"
+                    }
+                }
+            ]
+        }
+    });
+
+    let decoded = Message::Response(Response {
+        id: 123,
+        result: Some(Result::TextDocumentCompletionResult(CompletionResult {
+            items: vec![CompletionItem {
+                label: "some_label".to_string(),
+                insert_text: Some("some_insert_text".to_string()),
+                text_edit: Some(TextEdit {
+                    new_text: "some_new_text".to_string()
+                }),
+            }],
+        })),
+        error: None,
+    });
+
+    assert_json_decodes_into(json, decoded);
 }
