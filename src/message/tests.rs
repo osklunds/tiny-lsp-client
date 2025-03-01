f09623781de2022b6e269ec693abe612c46f7ced
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2025-03-01 15:48:24 +0100

    Check exp value in test

--- src/message/tests.rs
+++ src/message/tests.rs
@@ -18,5 +18,17 @@ fn completion_result() {
     let json: String = serde_json::to_string(&untyped).unwrap();
     let typed: Message = serde_json::from_str(&json).unwrap();
 
-    println!("oskar: {:?}", typed);
+    let exp = Message::Response(Response {
+        id: 123,
+        result: Some(Result::TextDocumentCompletionResult(CompletionResult {
+            items: vec![CompletionItem {
+                label: "some_label".to_string(),
+                insert_text: None,
+                text_edit: None,
+            }],
+        })),
+        error: None,
+    });
+
+    assert_eq!(exp, typed);
 }
