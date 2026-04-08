e6c2df6929f6b7d3f49634f55c6a88a477570d4f
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2026-04-08 15:26:24 +0000

    Add basic log file test

--- src/logger/tests.rs
+++ src/logger/tests.rs
@@ -19,6 +19,8 @@ use super::*;
 
 use regex::Regex;
 
+const TEST_LOG_FILE_NAME: &str = "test/logs/logger_tests.rs.log";
+
 #[test]
 fn timestamp() {
     let timestamp = get_timestamp();
@@ -26,11 +28,10 @@ fn timestamp() {
     println!("Timestamp: {}", timestamp);
     // No need to make it overly smart, because no matter how smart,
     // it will never be able to compare against the real time with a regex
-    let regex = Regex::new(
+    match_regex(
         r"^2[0-9]{3}-[01][0-9]-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}\.[0-9]{3}$",
-    )
-    .unwrap();
-    assert!(regex.is_match(&timestamp));
+        &timestamp,
+    );
 }
 
 #[test]
@@ -58,4 +59,39 @@ fn millisecond_padding() {
             }
         }
     }
+}
+
+// TODO: there is a lot more to test, like if a log is enabled or not, changing
+// log file, etc
+#[test]
+fn log_test() {
+    set_log_file_name(TEST_LOG_FILE_NAME);
+    log_io!("abc");
+    log_stderr!("def");
+    log_rust_debug!("hij");
+    log_emacs_debug!("klm");
+
+    let log_file_content = fs::read_to_string(TEST_LOG_FILE_NAME).unwrap();
+
+    match_regex(r"^IO - [0-9-]+ [0-9:.]+ - abc\n", &log_file_content);
+    match_regex(r"\nSTDERR - [0-9-]+ [0-9:.]+ - def\n", &log_file_content);
+    match_regex(
+        r"\nRUST_DEBUG - [0-9-]+ [0-9:.]+ - hij\n",
+        &log_file_content,
+    );
+    match_regex(
+        r"\nEMACS_DEBUG - [0-9-]+ [0-9:.]+ - klm\n$",
+        &log_file_content,
+    );
+
+    match_regex(
+        r"IO[^^]*STDERR[^^]*RUST_DEBUG[^^]*EMACS_DEBUG",
+        &log_file_content,
+    );
+}
+
+fn match_regex(regex: &str, subject: &str) {
+    let regex = Regex::new(regex).unwrap();
+
+    assert!(regex.is_match(subject));
 }
